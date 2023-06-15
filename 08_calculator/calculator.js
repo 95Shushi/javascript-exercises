3496e3c7c6bc1b5ac950c97d9e18b477b24bcce9
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-15 13:49:41 +0800

    Compelte 08 exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args.reduce((sum, item) => sum + item, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  return args.reduce((total, item) => total * item)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  factorialResult = 1
+	if (a === 0) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      factorialResult *= i;
+    }
+    return factorialResult;
+  }
 };
 
 // Do not edit below this line
