e4d4a85c4b7d3aa1fe108fcb0a0c4c3004056235
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-16 12:49:39 +0800

    Complete 09 exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    let array = string.toLowerCase().replaceAll(/[., !]/g, "").split("");
+    let reverseArray = string.toLowerCase().replaceAll(/[., !]/g, "").split("").reverse();
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== reverseArray[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
