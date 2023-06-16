67a2c5af5740d56808e316ce7f4f4cc1cf50e29c
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-16 13:10:17 +0800

    Complete 10 exercise, solution much nicer

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 1) return "OOPS";
+    if (n == 1) return 1;
+    firstNumber = 0;
+    secondNumber = 1;
+    sum = 0;
 
+    for (let i = 1; i < n; i++) {
+        sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// 1, 1, 2, 3, 5
+// 1, 2, 3, 4, 5
