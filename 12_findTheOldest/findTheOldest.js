0e919ac2dcd4a81995d16926fc06b225e17bbff6
Andrei <124041084+95Shushi@users.noreply.github.com>
2023-06-16 13:39:03 +0800

    12 exercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    const oldest = array.sort(function(a, b) {
+        if (a.yearOfDeath === undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (b.yearOfDeath === undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        if (lastGuy < nextGuy) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    console.table(oldest);
+    return oldest[oldest.length - 1]
 };
 
 // Do not edit below this line
